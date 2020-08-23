72544c1fa2a82ee3310a04016b428708a09586ab
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2020-08-23 19:56:39 +0530

    commit again

---
+++ src/components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () =>{
+    return(
+        <div>
+            <img alt='image' src=''/>
+            <h2>Name</h2>
+            <p>Email</p>
+        </div>
+    )
+}
+
+export default Card
