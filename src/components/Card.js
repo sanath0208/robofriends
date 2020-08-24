9392e3384111e04aef47b0b71d35e80532ca6eca
sanath0208 <53268864+sanath0208@users.noreply.github.com>
2020-08-24 09:31:51 +0530

    minor changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({id,name,email}) =>{
     return(
-        <div className='bg-light-green dib-ns br3 ma2 pa3 grow tc ma1 bw2 shadow-5'>
-            <img alt='robot' src={`https://robohash.org/${id}`}/>
+        <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+            <img alt='robot' src={`https://robohash.org/${id}?size=200x200`}/>
             <h2>{name}</h2>
             <p>{email}</p>
         </div>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchBox = ({onSearch}) => {
     return(
-        <div>
+        <div className='pa2'>
             <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={onSearch}></input>
         </div>
     )
